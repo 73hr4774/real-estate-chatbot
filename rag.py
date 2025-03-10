4fe1be4817b6977a3b66e349a1957e8aeb818ac9
unknown <khujwizehra@gmail.com>
2025-03-10 15:25:53 +0530

    changes made in requirements and rag.py

--- rag.py
+++ rag.py
@@ -1,3 +1,14 @@
+import os
+import sys
+import streamlit as st
+# Force Python to use pysqlite3 instead of old sqlite3
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+except ImportError:
+    pass  # pysqlite3 not installed, fallback to default
+
+
 from uuid import uuid4
 
 from dotenv import load_dotenv
@@ -9,7 +20,8 @@ from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from huggingface_hub import login
-import os
+
+
 
 load_dotenv()
 
@@ -17,7 +29,7 @@ CHUNK_SIZE=1000
 EMBEDDING_MODEL="Alibaba-NLP/gte-base-en-v1.5"
 VECTOR_STORE_DIR=Path(__file__).parent/"resources/vector_store"
 COLLECTION_NAME="real_estate"
-hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN") or st.secrets.get("HUGGINGFACEHUB_API_TOKEN")
 
 # Ensure token is set
 if not hf_token:
